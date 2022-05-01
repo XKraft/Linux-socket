ba5157dab926234ef498f84f1453f011d70ba8d3
lemontreeyi <yyi1725@foxmail.com>
2022-05-01 14:48:38 +0800

    by_yy

--- head/chatroom.h
+++ head/chatroom.h
@@ -1,22 +1,44 @@
 #ifndef _CHATROOM
 #define _CHATROOM
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdint.h>
+#include <string>
+using namespace std;
 typedef struct user_node
 {
-    char* name;
+    string name;
+    int userfd;
     int port;
+    user_node* next_user;
 } user_node;
 
+typedef struct file_node
+{
+    string filename;
+    int len;
+    file_node* next_file;
+} file_node;
 
 typedef struct chatroom
 {
     user_node* user_head;
     int number;
     bool send_flag;
-    char* msg;
+    file_node* file_head;
 } chatroom;
 
+//初始化聊天室
 extern void init_chatroom(chatroom* room);
-
+//查找用户
+extern user_node* find_user(chatroom* room, string name);
+//增加用户，增加时需要判断用户名是否重复
+extern void add_user(chatroom* room, string name, int port, int userfd);
+//删除用户
+extern void remove_user(chatroom* room, string name);
+//接收&保存文件->将字符数组写入文件
+extern void save_file(chatroom* room, uint8_t* str, int len, string filename);
+//发送文件->将服务端文件写入字符数组
+extern uint8_t* send_file(chatroom* room, string filename);
+//查找文件
+extern file_node* find_file(chatroom* room, string filename);
 #endif
--- src/chatroom.cpp
+++ src/chatroom.cpp
@@ -1,9 +1,118 @@
 #include"../head/chatroom.h"
+#include <iostream>
+#include <fstream>
 //初始化结构体chatroom
 void init_chatroom(chatroom* room)
 {
+    room->user_head = new user_node;
+    room->user_head->name = "";
+    room->user_head->port = 0;
+    room->user_head->userfd = 0;
+    room->user_head->next_user = NULL;
+
     room->number = 0;
+
     room->send_flag = false;
-    room->user_head = NULL;
-    room->msg = NULL;
+    
+    room->file_head = new file_node;
+    room->file_head->filename = "";
+    room->file_head->len = 0;
+    room->file_head->next_file = NULL;
+}
+
+//查找用户
+user_node* find_user(chatroom* room, string name)
+{
+    user_node* head = room->user_head;
+    user_node* p = NULL;
+    while(head->next_user != NULL)
+    {
+        p = head->next_user;
+        if(p->name == name) break;
+        head = p;
+    }
+    return p;
+}
+
+//增加用户的同时判断是否重复
+void add_user(chatroom* room, string name, int port, int userfd)
+{
+    bool flag = true;
+    user_node* head = room->user_head;
+    user_node* p = NULL;
+    while(head->next_user != NULL)      //判断是否有重复用户名
+    {
+        p = head->next_user;
+        if(p->name == name)
+        {
+            flag = false;
+            break;
+        }
+        head = p;
+    }
+    if(flag == true)    //头部插入
+    {
+        room->number += 1;
+        user_node* new_user = new user_node;
+        new_user->name = name;
+        new_user->port = port;
+        new_user->userfd = userfd;
+        new_user->next_user = room->user_head->next_user;
+        room->user_head->next_user = new_user;
+    }
+    else cout<<"该用户已存在，请勿重复登录..."<<endl;
+    head = p = NULL;
+}
+
+//删除用户
+void remove_user(chatroom* room, string name)
+{
+    user_node* head = room->user_head;
+    user_node* p = NULL;
+    while(head->next_user != NULL)
+    {
+        p = head->next_user;
+        if(p->name == name) break;
+        head = p;
+    }
+    head->next_user = p->next_user;
+    room->number -= 1;
+    delete p;
+    cout<<"已删除用户"<<name<<endl;
+    head = p = NULL;
+}
+
+//查找文件
+file_node* find_file(chatroom* room, string filename)
+{
+    file_node* head = room->file_head;
+    file_node* p = NULL;
+    while(head->next_file != NULL)
+    {
+        p = head->next_file;
+        if(p->filename == filename) break;
+        head = p;
+    }
+    return p;
+}
+
+//从本地打开文件并发送
+uint8_t* send_file(chatroom* room, string filename)
+{
+    string file_path = "./file_dir" + filename;
+    ifstream ifs;
+    ifs.open(file_path, ios::in);
+    if(!ifs.is_open())
+    {
+        cout<<"open file failed...";
+        return 0;
+    }
+    uint8_t* str = new uint8_t;
+    int num = 0;
+    char c;
+    while((c = ifs.get()) != EOF)
+    {
+        *(str+(num++)) = (uint8_t)c;
+    }
+    return str;
 }
